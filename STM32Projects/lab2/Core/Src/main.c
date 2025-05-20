a88946d36f39751dba5c24e3e7cafccb9d048d83
dgadrian05 <dgadrian05@gmail.com>
2025-05-20 16:46:50 +0300

    Wrote a code which starts with the red light turned on and after each time the button is pressed is going in the sequence:RED-RED/ORANGE-RED/ORANGE/BLUE-RED/ORANGE/BLUE/GREEN-AllTurnedOFF and repeat.

--- STM32Projects/lab2/Core/Src/main.c
+++ STM32Projects/lab2/Core/Src/main.c
@@ -1,21 +1,34 @@
 #include "stm32f3xx.h"
-//Implement a program that turns on each LED in turn. (Button is not used in this exercise)
 
 int main(void){
 
-	RCC->AHBENR |=(1UL<<18U);
+
+
+	RCC->AHBENR |=(1UL<<18U)|(1UL<<17U);
 	GPIOB->MODER=0X00055000;
+	//0x40-red
+	//0x80-blue
+	//0x100-orange
+	//0x200-green
+	//0x000 all off
+	//0x3C0 all on
 
-//int led_pins[] = {1UL << 6, 1UL << 8, 1UL << 7, 1UL << 9};
-int led_pins[] = {1UL << 9, 1UL << 7, 1UL << 8, 1UL << 6};
+int state = 0;
+int previous = 0;
 
-	while(1)
-	{
-		for(int i =0;i<4;i++)
-		{
-			GPIOB->ODR = led_pins[i];
-			for(int j =0;j<800000;j++);
-		}
-	}
-	return 0;
+    while (1) {
+        int now = GPIOA->IDR & 0x1;
+        if (now && !previous) {
+            state = (state + 1) % 5;
+        }
+        previous = now;
+
+        switch (state) {
+            case 0: GPIOB->ODR = 0x40; break;
+            case 1: GPIOB->ODR = 0x140; break;
+            case 2: GPIOB->ODR = 0x1C0; break;
+            case 3: GPIOB->ODR = 0x3C0; break;
+            case 4: GPIOB->ODR = 0x000; break;
+        }
+    }
 }
